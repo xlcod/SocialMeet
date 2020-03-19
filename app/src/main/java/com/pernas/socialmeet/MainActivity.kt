1b6c927cd2811adcf971980bb36cdc3190024412
xlcod <alvaro.pernas@live.u-tad.com>
2020-03-19 21:51:58 +0100

    added choose image from phone

--- app/src/main/java/com/pernas/socialmeet/MainActivity.kt
+++ app/src/main/java/com/pernas/socialmeet/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/pernas/socialmeet/RegistrerActivity.kt
+++ app/src/main/java/com/pernas/socialmeet/RegistrerActivity.kt
@@ -1,17 +1,20 @@
 package com.pernas.socialmeet
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Build.VERSION
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_registrer.*
+import java.util.jar.Manifest
 
 class RegistrerActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-// ...
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +26,60 @@ class RegistrerActivity : AppCompatActivity() {
 
         signUpButton.setOnClickListener {
             signUpUser()
+        }
+        buttonImagePicker.setOnClickListener {
+            if (VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    //denied
+                    val permissions = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISION_CODE);
+                } else {
+                    //granted
+                    pickImageFromGallery();
+                }
 
+            } else {
+                //system os < marshmallow
+            }
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+
+    }
+
+    companion object {
+        //image picker
+        private val IMAGE_PICK_CODE = 1000;
+        //Permision code
+        private val PERMISION_CODE = 1001;
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImageFromGallery();
+                } else {
+                    Toast.makeText(this, "Permission DENIED ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+            imageProfile.setImageURI(data?.data)
         }
     }
 
