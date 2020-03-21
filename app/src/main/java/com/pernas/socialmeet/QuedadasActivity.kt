fa5ac45f59fcb66d36ee6f9188d981ff1d1bb26a
Alvaro <xlcodfree@gmail.com>
2020-03-21 18:25:43 +0100

    log out working

--- app/src/main/java/com/pernas/socialmeet/QuedadasActivity.kt
+++ app/src/main/java/com/pernas/socialmeet/QuedadasActivity.kt
@@ -1,12 +1,29 @@
 package com.pernas.socialmeet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_quedadas.*
 
 class QuedadasActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quedadas)
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        logOutIcon.setOnClickListener {
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        FirebaseAuth.getInstance().signOut()
+        finish()
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
